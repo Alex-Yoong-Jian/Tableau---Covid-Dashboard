b48ac4a6831cb4cd8e8505ddc124f9156e0ef513
Alex-Yoong-Jian <108650906+Alex-Yoong-Jian@users.noreply.github.com>
2022-07-05 20:14:22 +0800

    Add files via upload
    These are the files that are related to the Covid Dashboard on Tableau (Refer to this link for the end-product: https://public.tableau.com/app/profile/alex.yoong.jian/viz/CovidDashboard_16570224009120/Dashboard1?publish=yes)

---
+++ Tableau SQL Query.sql
@@ -0,0 +1,47 @@
+/*
+Queries used for Tableau Project
+*/
+
+
+
+-- 1. 
+
+-- Getting the global percentage of covid related deaths
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+where continent is not null 
+order by 1,2
+
+
+-- 2. 
+
+-- Excluding non-relevant data in the condition
+-- European Union is part of Europe, thus it is excluded as it is already counted in Europe
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International', 'Upper middle income', 'High income', 'Lower middle income', 'Low income')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3.
+
+-- Getting the percentage of population infected in each location per population
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4.
+
+-- Getting the percentage of population infected per population by date series
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
